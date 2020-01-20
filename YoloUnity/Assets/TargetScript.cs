1ff4ecc75e9369a5b544f55d78a2f4c312c4720b
Vladimir Vasilev <v.vasilev@synergysportstech.com>
2020-01-20 22:06:35 +0200

    Added sphere and changed YOLO objects to have X coordinate of 100 to prepare for split screen

---
+++ YoloUnity/Assets/TargetScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private const float _speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.A))
+      {
+        transform.Translate(-_speed,0,0);
+      } else if (Input.GetKey(KeyCode.D))
+      {
+        transform.Translate(_speed,0,0);
+      } else if (Input.GetKey(KeyCode.W))
+      {
+        transform.Translate(0,_speed,0);
+      } else if (Input.GetKey(KeyCode.S))
+      {
+        transform.Translate(0,-_speed,0);
+      }
+    }
+}
