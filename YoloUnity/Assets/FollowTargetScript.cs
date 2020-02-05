29baf9921d1b791e4592bde8d8be03ecce522069
Vladimir Vasilev <v.vasilev@synergysportstech.com>
2020-02-05 22:15:38 +0200

    Cleaned rotation, move and follow scripts and made their parameters Unity-configurable

--- YoloUnity/Assets/FollowTargetScript.cs
+++ YoloUnity/Assets/FollowTargetScript.cs
@@ -3,6 +3,7 @@
 public class FollowTargetScript : MonoBehaviour
 {
     public Transform _followedObject = null;
+    public int _secondsBeforeFollowingStarts = 1;
     public int _speed = 10;
 
     // Start is called before the first frame update
@@ -13,8 +14,7 @@ public class FollowTargetScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Time.fixedTime < 1) return;
-        var distance = _followedObject.position - transform.position;
+        if (Time.fixedTime < _secondsBeforeFollowingStarts) return;
 
         transform.position =  Vector3.MoveTowards(transform.position, _followedObject.transform.position, _speed * Time.deltaTime);
 
--- YoloUnity/Assets/HelicopterRotationScript.cs
+++ YoloUnity/Assets/HelicopterRotationScript.cs
@@ -2,41 +2,37 @@
 
 public class HelicopterRotationScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    private const float _speed = 0.5f;
+    public float _moveSpeed = 0.5f;
+    public float _rotationSpeed = 500;
 
     // Update is called once per frame
     void Update()
     {
       if (Input.GetKey(KeyCode.A))
       {
-        transform.Translate(-_speed,0,0);
+        transform.Translate(-_moveSpeed,0,0);
       } else if (Input.GetKey(KeyCode.D))
       {
-        transform.Translate(_speed,0,0);
+        transform.Translate(_moveSpeed,0,0);
       } else if (Input.GetKey(KeyCode.W))
       {
-        transform.Translate(0,_speed,0);
+        transform.Translate(0,_moveSpeed,0);
       } else if (Input.GetKey(KeyCode.S))
       {
-        transform.Translate(0,-_speed,0);
+        transform.Translate(0,-_moveSpeed,0);
       }
 
       if (Input.GetKeyDown(KeyCode.L)) {
-        transform.RotateAround(transform.position, Vector3.up, -500 * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.up, -_rotationSpeed * Time.deltaTime);
       }
       if (Input.GetKeyDown(KeyCode.J)) {
-        transform.RotateAround(transform.position, Vector3.up, -500 * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.up, _rotationSpeed * Time.deltaTime);
       }
       if (Input.GetKeyDown(KeyCode.I)) {
-        transform.RotateAround(transform.position, Vector3.right, -500 * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.right, -_rotationSpeed * Time.deltaTime);
       }
       if (Input.GetKeyDown(KeyCode.K)) {
-        transform.RotateAround(transform.position, Vector3.right, 500 * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.right, _rotationSpeed * Time.deltaTime);
       }
     }
 }
